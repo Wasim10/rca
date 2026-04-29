9c19e121c41eba8a1ad745c09c8f02e936eeffba
Waseem <Waseem@MacBook-Pro.local>
2026-04-29 10:08:02 -0700

    Add complete React Native app with authentication and tabs

---
+++ app/(tabs)/subscripions.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const subscripions = () => {
+  return (
+    <View>
+      <Text>subscripions</Text>
+    </View>
+  )
+}
+
+export default subscripions
---
+++ app/onboarding.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+
+const onboarding = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text>onboarding</Text>
+
+      {/*   <Link href="/testf" className="mt-4 rounded bg-primary p-4  text-white" >Go to Test f...
+      </Link>
+      */}
+    </View>
+  )
+}
+
+
+export default onboarding
